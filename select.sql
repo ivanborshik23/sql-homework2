8928f0dd5b464f7e634884cef1a6b97441682cb0
ivanborshik23 <ivanborshik23@gmail.com>
2026-02-24 17:53:34 +0300

    Add select queries (tasks 2, 3, 4)

--- select.sql
+++ select.sql
@@ -3,37 +3,45 @@ FROM track
 ORDER BY duration DESC
 LIMIT 1;
 
+
 SELECT name
 FROM track
 WHERE duration >= 210;
+
 
 SELECT name
 FROM compilation
 WHERE year BETWEEN 2018 AND 2020;
 
+
 SELECT name
 FROM artist
 WHERE name NOT LIKE '% %';
+
 
 SELECT name
 FROM track
 WHERE LOWER(name) LIKE '%my%'
    OR LOWER(name) LIKE '%мой%';
 
+
 SELECT g.name, COUNT(ga.artist_id) AS artist_count
 FROM genre g
 LEFT JOIN genre_artist ga ON g.id = ga.genre_id
 GROUP BY g.name;
+
 
 SELECT COUNT(t.id)
 FROM track t
 JOIN album a ON t.album_id = a.id
 WHERE a.year BETWEEN 2019 AND 2020;
 
+
 SELECT a.name, AVG(t.duration) AS avg_duration
 FROM album a
 JOIN track t ON a.id = t.album_id
 GROUP BY a.name;
+
 
 SELECT name
 FROM artist
@@ -44,6 +52,7 @@ WHERE id NOT IN (
     WHERE a.year = 2020
 );
 
+
 SELECT DISTINCT c.name
 FROM compilation c
 JOIN compilation_track ct ON c.id = ct.compilation_id
@@ -53,6 +62,7 @@ JOIN album_artist aa ON a.id = aa.album_id
 JOIN artist ar ON aa.artist_id = ar.id
 WHERE ar.name = 'Muse';
 
+
 SELECT a.name
 FROM album a
 JOIN album_artist aa ON a.id = aa.album_id
@@ -60,17 +70,20 @@ JOIN genre_artist ga ON aa.artist_id = ga.artist_id
 GROUP BY a.name
 HAVING COUNT(DISTINCT ga.genre_id) > 1;
 
+
 SELECT name
 FROM track
 WHERE id NOT IN (
     SELECT track_id FROM compilation_track
 );
 
+
 SELECT ar.name
 FROM artist ar
 JOIN album_artist aa ON ar.id = aa.artist_id
 JOIN track t ON aa.album_id = t.album_id
 WHERE t.duration = (SELECT MIN(duration) FROM track);
+
 
 SELECT a.name
 FROM album a
